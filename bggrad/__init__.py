ec44c279bdeb6d2cebe6e5635c330cb5ac315233
Håkan x Ardö <hakanad@axis.com>
2017-11-27 16:17:32 +0100

    during setup we cant import BgGrad
    Change-Id: Iaf1d50a14b1cb8d34f580f0b09a590ad37239e0f

--- bggrad/__init__.py
+++ bggrad/__init__.py
@@ -1,4 +1,7 @@
-from bggrad.api import BgGrad
+try:
+    from bggrad.api import BgGrad
+except ImportError:
+    pass
 
 __version_info__ = (1, 0, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
