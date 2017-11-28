f7c8d84c7b75ca80c639b7fa7e511a5032f95df2
Håkan x Ardö <hakanad@axis.com>
2017-11-28 08:16:28 +0100

    info message
    Change-Id: I6474d175eb45b843ded687d57a6cbfb5bc861dde

--- bggrad/__init__.py
+++ bggrad/__init__.py
@@ -1,7 +1,7 @@
 try:
     from bggrad.api import BgGrad
 except ImportError:
-    pass
+    print("Use build_* scripts to build C extention or use setup.py to install.")
 
 __version_info__ = (1, 0, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
